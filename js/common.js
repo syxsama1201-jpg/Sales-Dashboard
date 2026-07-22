add102b9ab04a5695bfdfb961bef69c53b1b9522
syxsama1201-jpg <syxsama1201@gmail.com>
2026-07-22 14:15:05 +0800

    nas-v2.0

--- js/common.js
+++ js/common.js
@@ -5,7 +5,7 @@
 
 // ==================== 全局常量 ====================
 const AUTH_KEY = 'salesReport_auth';
-const API_BASE = 'http://192.168.50.105:5000';
+const API_BASE = '';
 const FRONT_PAGE = 'index.html';
 const PAGE_ACCESS_MAP = {
     'sales.html': 'sales',
